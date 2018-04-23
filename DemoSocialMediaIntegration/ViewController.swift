12339a23a0e984c266b58a73cbc41f191e78ef3c
saurabh <saurabhbsd@gmail.com>
2018-04-23 17:12:22 +0530

    initial commit

---
+++ DemoSocialMediaIntegration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoSocialMediaIntegration
+//
+//  Created by Macbook Air on 23/04/18.
+//  Copyright © 2018 Saurabh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

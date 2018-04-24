c99223f6dc9772939ab9b07efef4e2d4d89ca678
Saurabh Bajaj <saurabhbsd@gmail.com>
2018-04-24 18:39:41 +0530

    fix(login): not navigating to home screen on successful facebook login feat(login): create login options view class refactor(login): refactor LoginOptionsViewController class
    - check login status in view did appear funtion so that app navigates to home screen after facebook login screen is dismissed.
    - initialise facebook login button in login options view class
    - in LoginOptionsViewController, reduce redundancy and add comments wherever needed.
    
    Signed-off-by: Saurabh Bajaj <saurabhbsd@gmail.com>

---
+++ DemoSocialMediaIntegration/Login/LoginOptionsView.swift
@@ -0,0 +1,23 @@
+//
+//  LoginOptionsView.swift
+//  DemoSocialMediaIntegration
+//
+//  Created by Macbook Air on 24/04/18.
+//  Copyright © 2018 Saurabh. All rights reserved.
+//
+
+import UIKit
+import FBSDKLoginKit
+
+class LoginOptionsView: UIView {
+    @IBOutlet weak var fbLoginButton: FBSDKLoginButton!
+    
+    override func awakeFromNib() {
+        setupSubviews()
+    }
+    
+    // MARK: helpers
+    private func setupSubviews() {
+        fbLoginButton.readPermissions = ["public_profile", "email"]
+    }
+}
--- DemoSocialMediaIntegration/Login/LoginOptionsViewController.swift
+++ DemoSocialMediaIntegration/Login/LoginOptionsViewController.swift
@@ -11,21 +11,35 @@ import FBSDKLoginKit
 import FBSDKCoreKit
 
 class LoginOptionsViewController: UIViewController {
-
-    @IBOutlet weak var fbLoginButton: FBSDKLoginButton!
+    
+    private var isLoggedIn: Bool {
+        if (FBSDKAccessToken.current() != nil) {
+            return true
+        }
+        return false
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
-        
-        fbLoginButton.readPermissions = ["public_profile", "email"]
         
         // register for facebook login status notifications
         registerForFacebookTokenChange()
         
         // check if user is already logged in using facebook
-        if (FBSDKAccessToken.current() != nil) {
+        if isLoggedIn {
+            // show home screen
+            showHomeScreen()
+        }
+    }
+    
+    /** override viewDidAppear to check current login status after facebook login screen is dismissed.
+     */
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // check if user is already logged in using facebook
+        if isLoggedIn {
             // show home screen
             showHomeScreen()
         }
@@ -56,10 +70,7 @@ class LoginOptionsViewController: UIViewController {
     /// navigates to home screen
     private func showHomeScreen() {
         DispatchQueue.main.async {
-//            self.performSegue(withIdentifier: "showHome", sender: self)
-            let alert = UIAlertController(title: "Success", message: "Logged In", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-            self.show(alert, sender: nil)
+            self.performSegue(withIdentifier: "showHome", sender: self)
         }
     }
 }
